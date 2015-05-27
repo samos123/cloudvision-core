d501ceaa83c5b0f8a453f2c1c10a6c5131da7515
Sam Stoelinga <sammiestoel@gmail.com>
2015-05-27 18:50:54 +0800

    Script to first download binaries to local pc

---
+++ download-binaries.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://mirrors.cnnic.cn/apache/hadoop/core/hadoop-2.6.0/hadoop-2.6.0.tar.gz 
+
+wget http://mirrors.cnnic.cn/apache/spark/spark-1.3.1/spark-1.3.1-bin-hadoop2.6.tgz
