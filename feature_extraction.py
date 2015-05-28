437f6af26cbe5579fc115212ce63b8969bb6cf36
Sam Stoelinga <sammiestoel@gmail.com>
2015-05-28 20:50:49 +0800

    Added ansible tasks to install opencv with contrib module

--- feature_extraction.py
+++ feature_extraction.py
@@ -7,12 +7,13 @@ from pyspark import SparkContext
 
 
 
-def extract_sift_features_opencv(imgfilename, imgbytes):
+def extract_sift_features_opencv(imgfile_imgbytes):
+    imgfilename, imgbytes = imgfile_imgbytes
     nparr = np.fromstring(imgbytes, np.uint8)
     imarr = cv2.imdecode(imgbytes,cv2.CV_LOAD_IMAGE_COLOR)
 
     gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    sift = cv2.SIFT()
+    sift = cv2.xfeatures2d.SIFT_create()
 
     kp, descriptors = sift.detectAndCompute(gray, None)
     
@@ -22,7 +23,9 @@ def extract_sift_features_opencv(imgfilename, imgbytes):
 if __name__ == "__main__":
     sc = SparkContext(appName="feature_extractor")
 
-    seqFilePath = sys.args[1]
+    seqFilePath = sys.argv[1]
     images = sc.sequenceFile(seqFilePath)
 
-    images_sift = images.map(extract_and_save_sift_features)
+    images_sift = images.map(extract_sift_features_opencv)
+
+    images_sift.saveAsSequenceFile("hdfs://192.168.33.10/tmp/seq-sift")
---
+++ scripts/upload-to-sequence-file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+java -cp target/SamKmeans-1.2-jar-with-dependencies.jar sam.UploadToSequenceFile "$1" "$2"
